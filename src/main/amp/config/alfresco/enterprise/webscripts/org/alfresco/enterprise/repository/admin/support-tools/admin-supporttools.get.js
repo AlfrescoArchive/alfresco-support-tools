328f0fd946eb900918a4c18c921f5d64016febb5
Antonio Soler <morgalad.es@gmail.com>
2015-05-13 15:14:05 +0100

    Delete admin-supporttools.get.js

--- src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/support-tools/admin-supporttools.get.js
+++
@@ -1,9 +0,0 @@
-<import resource="classpath:alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/admin-common.lib.js">
-
-/**
- * Repository Admin Console
- * 
- * Support Tools GET method
- */
-model.tools = Admin.getConsoleTools("admin-supporttools");
-model.metadata = Admin.getServerMetaData();
